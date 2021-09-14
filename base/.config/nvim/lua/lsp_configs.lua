8fbe3c8c642c08bdc2d7b029998f280fdc0803ad
João Fukuda <joao.fukuda@protonmail.com>
2021-09-14 11:53:14 -0300

    [+] Update README.md programs to reflect recent changes

--- base/.config/nvim/lua/lsp_configs.lua
+++ base/.config/nvim/lua/lsp_configs.lua
@@ -4,5 +4,5 @@ vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
 --require('lspconfig').clangd.setup{ on_attach = require'completion'.on_attach; }
 --require('lspconfig').pyls.setup{ on_attach = require'completion'.on_attach; }
 --require('lspconfig').erlangls.setup{ on_attach = require'completion'.on_attach; }
---require('lspconfig').cmake.setup{ on_attach = require'completion'.on_attach; }
+require('lspconfig').cmake.setup{ on_attach = require'completion'.on_attach; }
 
